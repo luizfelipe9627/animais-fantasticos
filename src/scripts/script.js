a1034ffd3d322cdaf80bbc45286c9c2124d2ef98
Luiz Felipe Silva <felipesilva9627@hotmail.com>
2023-06-15 10:11:40 -0300

    ScrollSuave transformado em classe

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,5 +1,5 @@
 // O import é usado para chamar uma função por exemplo para importar dinamicamente um módulo que utiliza export.
-import initScrollToSection from "./modules/scroll-suave.js";
+import ScrollSuave from "./modules/scroll-suave.js";
 import initAnimationScroll from "./modules/scroll-animacao.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tab-nav.js";
@@ -11,8 +11,11 @@ import initFuncionamento from "./modules/funcionamento.js";
 import initFetchAnimais from "./modules/fetch-animais.js";
 import initFetchBitcoin from "./modules/fetch-bitcoin.js";
 
+
 // Está executando as funções.
-initScrollToSection();
+const scrollSuave = new ScrollSuave("[data-menu='smooth'] a[href^='#']"); // Está criando um objeto e passando o link do menu como parâmetro.
+scrollSuave.init(); // Está chamando o método init do objeto criado.
+
 initAnimationScroll();
 initAccordion();
 initTabNav();
